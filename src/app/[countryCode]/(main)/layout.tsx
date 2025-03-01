f939e3688f0320cc67c0ec61d114bc2aafec0e79
Code Head <codehead@codelegends.com>
2025-03-01 01:07:27 -0500

    removed unnecessary footer

--- src/app/[countryCode]/(main)/layout.tsx
+++ src/app/[countryCode]/(main)/layout.tsx
@@ -5,7 +5,6 @@ import { retrieveCustomer } from "@lib/data/customer"
 import { getBaseURL } from "@lib/util/env"
 import { StoreCartShippingOption } from "@medusajs/types"
 import CartMismatchBanner from "@modules/layout/components/cart-mismatch-banner"
-import Footer from "@modules/layout/templates/footer"
 import FreeShippingPriceNudge from "@modules/shipping/components/free-shipping-price-nudge"
 
 export const metadata: Metadata = {
@@ -37,7 +36,6 @@ export default async function PageLayout(props: { children: React.ReactNode }) {
         />
       )}
       {props.children}
-      <Footer />
     </>
   )
 }
